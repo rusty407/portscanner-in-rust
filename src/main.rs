d9b282eaae01b9b6c821b10fde1f268398b20c8a
Rusty407 <sandroamiridze93@gmail.com>
2026-01-06 00:28:13 -0500

    added arguments that make this scanner quicker

--- src/main.rs
+++ src/main.rs
@@ -66,6 +66,8 @@ impl PortScanner {
             Ok(Ok(_)) => Some(port),
             _ => None,
         }
+
+
     }
 }
 
@@ -82,6 +84,59 @@ fn print_usage() {
     println!("  Add 'fast' to use aggressive settings:          # 50ms timeout, 15000 concurrent");
     println!("  cargo run -- <ip> all fast");
 }
+
+fn parse_args() -> Result<(IpAddr, u16, u16, Duration, usize), String> {
+let args: Vec<String> = env::args().collect();
+
+if args.len() < 3 {
+    return Err("You need more arguments".to_string());
+}
+
+let target = args[1].parse::<IpAddr>()
+    .map_err(|_| format!("Invalid Ip address: {}" args[1]))?;
+
+let (start_port, end_port) = if args.len() == 3 || (args.len() > 3 && args[3] == "fast") {
+    match args[2].to_lowercase().as_str() {
+        "all" => (1u16, 65535u16),
+        "common" => (1u16, 1000u16),
+        _ => return Err("Invalid argument. Use 'all', 'common', or specify start and end ports".to_string()),
+    }
+} else {
+    let start = args[2].parse::<u16>()
+        .map_err(|_| format!("Invalid start port: {}", args[2]))?;
+    let end = args[3].parse::<u16>()
+        .map_err(|_| format!("Invalid end port: {}", args[3]))?;
+
+    if start > end {
+        return Err("Start port must be <= end port".to_string());
+    }
+    if start == 0 {
+        return Err("Port 0 is not valid".to_string());
+    }
+
+    (start, end)
+};
+
+let is_fast_mode = args.iter().any(|arg| arg == "fast");
+
+let timeout = if is_fast_mode {
+    Duration::form_millis(50)
+} else if args.len() > 4 && args[4] != "fast" {
+    Duration::from_millis(args[4].parse::<u64>()
+        .map_err(|_| format!("Invalid timeout: {}", args[4]))?)
+} else {
+    Duration::from_millis(50)
+};
+
+let concurrent = if is_fast_mode {
+    15000
+} else if args.len() > 5 {
+    args[5].parse::<usize>()
+        .map_err(|_| format!("Invalid concurrent limit: {}", args[5]))?;
+} else {
+    10000
+};
+ok((target, start_port, end_port, timeout, concurrent))
 
 #[tokio::main]
 async fn main() {
